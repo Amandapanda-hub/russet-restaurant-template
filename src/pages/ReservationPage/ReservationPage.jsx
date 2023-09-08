f53d260e2bfb35b0c3ead3d0f0154e03bbcffae1
Amanda Hilton <114438874+Amandapanda-hub@users.noreply.github.com>
2023-09-08 11:14:11 -0600

    adding calling info at bottom of reservations page

--- src/pages/ReservationPage/ReservationPage.jsx
+++ src/pages/ReservationPage/ReservationPage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import ReservationHero from './components/ReservationHero/ReservationHero'
 import ReservationMaker from './components/ReservationMaker/ReservationMaker'
+import ReservationCalling from './components/ReservationCalling/ReservationCalling'
 
 export default function ReservationPage() {
   return (
     <>
         <ReservationHero/> 
         <ReservationMaker/>
+        <ReservationCalling/>
     </>
   )
 }
--- src/pages/ReservationPage/components/ReservationCalling/ReservationCalling.jsx
+++ src/pages/ReservationPage/components/ReservationCalling/ReservationCalling.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 export default function ReservationCalling() {
   return (
-    <div>ReservationCalling</div>
-  )
+    <div className="p-20 md:p-30 text-center space-y-10">
+        <h2 className="text-5xl w-2/3 mx-auto">
+          CALL (555) 555-5555 TO RESERVE A ROOM FOR YOUR RECEPTION OR PARTY.
+        </h2>
+    </div>
+  );
 }
