48ddfffa06d83041519ec179e4561bc70337273c
Amanda Hilton <114438874+Amandapanda-hub@users.noreply.github.com>
2023-09-08 17:49:18 -0600

    fixing reservation from black to blue

--- src/pages/ReservationPage/components/ReservationCalling/ReservationCalling.jsx
+++ src/pages/ReservationPage/components/ReservationCalling/ReservationCalling.jsx
@@ -4,7 +4,7 @@ export default function ReservationCalling() {
   return (
     <div className="p-20 md:p-30 text-center space-y-10">
         <h2 className="text-5xl w-2/3 mx-auto">
-          CALL (555) 555-5555 TO RESERVE A ROOM FOR YOUR RECEPTION OR PARTY.
+          CALL <span className="text-[#262b4d] underline">(555) 555-5555</span> TO RESERVE A ROOM FOR YOUR RECEPTION OR PARTY.
         </h2>
     </div>
   );
