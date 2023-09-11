38a720fcaf8976090a1261fc85a0ecb1e3de5101
Amanda Hilton <114438874+Amandapanda-hub@users.noreply.github.com>
2023-09-11 13:27:06 -0600

    editing navbar / mobile navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import {BsYelp, BsGithub} from 'react-icons/bs'
-import {FaFacebook} from 'react-icons/fa'
+import { BsYelp, BsGithub, BsList, BsX } from 'react-icons/bs';
+import { FaFacebook } from 'react-icons/fa';
 
 export default function NavBar() {
   const [scrollPosition, setScrollPosition] = useState(0);
@@ -23,42 +23,51 @@ export default function NavBar() {
   } else if (scrollPosition >= 50 && scrollPosition < 100) {
     navStyle += ' bg-transparent';
   } else {
-    navStyle += ' -translate-y-full'; // This will move the navbar out of view
+    navStyle += ' -translate-y-full';
+  }
+
+  let textColor = "text-white";
+  if (scrollPosition >= 50 && scrollPosition < 100) {
+    textColor = "text-black"; 
   }
 
   const location = useLocation();
 
   return (
     <div className={navStyle}>
-      <div className="flex justify-between items-center text-white p-5">
-        <Link to='/' className="text-white text-lg uppercase">Russet</Link>
-        
+      <div className={`flex justify-between items-center p-5 ${textColor}`}>
+        <Link to='/' className={`text-lg uppercase ${isMobileMenuOpen ? 'text-black' : textColor}`}>Russet</Link>
+
         <div className="md:hidden">
           {isMobileMenuOpen ? (
-            <div className="absolute top-0 right-0 w-64 bg-white p-4 h-screen">
-              <button onClick={() => setIsMobileMenuOpen(false)}>X</button>
-              <Link to="/" activeClassName="underline block my-2">Home</Link>
-              <Link to="/menu" activeClassName="underline block my-2">Menu</Link>
-              <Link to="/reservation" activeClassName="underline block my-2">Reservation</Link>
-              <Link to='/locations' activeClassName="underline block my-2">Locations</Link> 
-              {/* Other Links */}
-              {/* React Icons */}
-              <BsYelp className="mt-4" />
-              <BsGithub className="mt-4" />
-              <FaFacebook className="mt-4" />
+            <div className="absolute top-0 left-0 w-full h-screen bg-white flex flex-col items-center justify-center space-y-8">
+              <Link to="/" className="text-black">Home</Link>
+              <Link to="/menu" className="text-black">Menu</Link>
+              <Link to="/reservation" className="text-black">Reservation</Link>
+              <Link to="/locations" className="text-black">Locations</Link>
+              <div className="flex space-x-4 mt-8">
+                <BsYelp className="text-black" />
+                <BsGithub className="text-black" />
+                <FaFacebook className="text-black" />
+              </div>
+              <div className="absolute top-5 right-10">
+                <button onClick={() => setIsMobileMenuOpen(false)}>
+                  <BsX size={32} className="text-black" />
+                </button>
+              </div>
             </div>
           ) : (
-            <button onClick={() => setIsMobileMenuOpen(true)}>Hamburger Icon</button>
+            <button onClick={() => setIsMobileMenuOpen(true)}>
+              <BsList size={32} className={textColor} />
+            </button>
           )}
         </div>
 
-        <div className="hidden md:flex space-x-4 md:space-x-6 lg:space-x-8">
-        <Link to="/" style={{ textDecoration: location.pathname === "/" ? "underline" : "none" }}>Home</Link>
+        <div className={`hidden md:flex space-x-4 md:space-x-6 lg:space-x-8 ${textColor}`}>
+          <Link to="/" style={{ textDecoration: location.pathname === "/" ? "underline" : "none" }}>Home</Link>
           <Link to="/menu" style={{ textDecoration: location.pathname === "/menu" ? "underline" : "none" }}>Menu</Link>
           <Link to="/reservation" style={{ textDecoration: location.pathname === "/reservation" ? "underline" : "none" }}>Reservation</Link>
           <Link to="/locations" style={{ textDecoration: location.pathname === "/locations" ? "underline" : "none" }}>Locations</Link>
-
-          {/* Other Links */}
           <BsYelp className="ml-4 self-center" />
           <BsGithub className="ml-4 self-center" />
           <FaFacebook className="ml-4 self-center" />
