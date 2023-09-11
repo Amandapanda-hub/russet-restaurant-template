181587f7dcf192fad92f41c386dfa174d4a9275b
Amanda Hilton <114438874+Amandapanda-hub@users.noreply.github.com>
2023-09-11 14:26:12 -0600

    fixing wordbreak section below hero

--- src/pages/Home/components/WordBreak/WordBreak.jsx
+++ src/pages/Home/components/WordBreak/WordBreak.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default function WordBreak() {
   return (
-    <div className="p-20 md:p-30 text-center space-y-10">
-      <h2 className="text-5xl">A DESTINATION</h2>
-      <p className="text-xl w-1/2 mx-auto">Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Nullam quis risus eget urna mollis ornare vel eu leo. Aenean lacinia bibendum nulla sed consectetur.</p>
+    <div className="p-4 md:p-20 lg:p-30 text-center space-y-4 md:space-y-10">
+      <h2 className="text-2xl md:text-4xl lg:text-5xl">A DESTINATION</h2>
+      <p className="text-sm md:text-xl w-full md:w-3/4 lg:w-1/2 mx-auto">Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Nullam quis risus eget urna mollis ornare vel eu leo. Aenean lacinia bibendum nulla sed consectetur.</p>
     </div>
   )
 }
